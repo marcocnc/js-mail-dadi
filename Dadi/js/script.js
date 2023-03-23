0cf28cdb8f4132bf0c955d4185d87cc0fa978a55
marcocnc <marcocancelliere56@gmail.com>
2023-03-23 22:09:44 +0100

    stamped in page and created layout

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -3,15 +3,38 @@ const arrayCPU = [1, 2, 3, 4, 5, 6];
 const arrayUser = [1, 2, 3, 4, 5, 6];
 console.log(arrayCPU, arrayUser);
 
-const randomCPU = Math.floor(Math.random() * arrayCPU.length);
-const randomUser = Math.floor(Math.random() * arrayUser.length);
-console.log(randomCPU, randomUser);
 
-if(randomCPU > randomUser){
-    console.log('Vince CPU');
-}else if(randomCPU === randomUser){
-    console.log('Pareggio');
-}else{
-    console.log('Vince User');
 
-}
+const btnPlay = document.getElementById('play');
+btnPlay.addEventListener('click', function(){
+    
+    //Creazione numeri random
+    const randomCPU = Math.floor(Math.random() * arrayCPU.length);
+    const randomUser = Math.floor(Math.random() * arrayUser.length);
+    console.log(randomCPU, randomUser);
+
+    //Da stampare
+    const outputMessage = document.getElementById('message');
+    const outputCPU = document.getElementById('numberCPU');
+    const outputUser = document.getElementById('numberUser');
+    
+    //Array stampati
+    outputCPU.innerHTML = arrayCPU[randomCPU];
+    outputUser.innerHTML = arrayUser[randomUser];
+    console.log(outputCPU, outputUser);
+
+    /*Condizioni per visualizzare diversi messaggi in colori diversi, aggiungendo la classe desiderata e rimuovendo le altre, perché altrimenti continuerebbero ad aggiungersi e si bugga*/
+    if(randomCPU > randomUser){
+        outputMessage.innerHTML = "Hai perso :(";
+        outputMessage.classList.add('defeat');
+        outputMessage.classList.remove('draw', 'victory');
+    }else if(randomCPU === randomUser){
+        outputMessage.innerHTML = "Pareggio :l";
+        outputMessage.classList.add('draw');
+        outputMessage.classList.remove('victory', 'defeat');
+    }else{
+        outputMessage.innerHTML = "Hai vinto! :D";
+        outputMessage.classList.add('victory');
+        outputMessage.classList.remove('draw', 'defeat');
+    }
+})
